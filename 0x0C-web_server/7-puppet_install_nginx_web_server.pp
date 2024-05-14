0769594f51bf155c01f8a4c3850792b097a35f5d
Kirui Brian <brinkiru@gmail.com>
2024-05-14 18:13:42 +0300

    add corrected 7-puppet_install_nginx_web_server.pp file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,14 +2,45 @@
 class nginx_setup {
   package { 'nginx':
     ensure => installed,
+    before => File['/var/www/html/index.html'],
   }
 
   service { 'nginx':
     ensure     => running,
     enable     => true,
     require    => Package['nginx'],
+    subscribe  => File['/var/www/html/index.html', '/etc/nginx/sites-available/default'],
   }
 
   file { '/var/www/html/index.html':
     ensure  => file,
-    content => '<html>\n<head>\n<title>Hello World
+    content => 'Hello World!\n',
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => template('nginx/default.erb'),
+    require => Package['nginx'],
+  }
+}
+
+include nginx_setup
+
+# Template file: nginx/default.erb
+# Place this template in the appropriate Puppet module directory
+<% | $redirect_url = 'http://google.com/' |%>
+server {
+  listen 80;
+  listen [::]:80 default_server;
+  root   /var/www/html;
+  index  index.html index.htm index.nginx-debian.html;
+
+  location / {
+    try_files $uri $uri/ =404;
+  }
+
+  location /redirect_me {
+    return 301 <%= $redirect_url %>;
+  }
+}
