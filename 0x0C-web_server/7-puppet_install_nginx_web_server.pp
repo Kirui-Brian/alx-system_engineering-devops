d69bac328d978eff749922ba761cec2edacae7d4
Kirui Brian <brinkiru@gmail.com>
2024-05-16 10:44:08 +0300

    update  7-puppet_install_nginx_web_server.pp file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,35 @@
-# Script that installs and configures Nginx
-exec {'update':
-  provider => shell,
-  path     => '/usr/bin:/usr/sbin:/bin',
-  command  => 'sudo apt-get -y update',
+# Puppet manifest to install and configure Nginx with a 301 redirect for /redirect_me
+
+class nginx_setup {
+  package { 'nginx':
+    ensure => installed,
+    before => File['/var/www/html/index.html'],
+  }
+
+  file { '/var/www/html/index.html':
+    ensure  => file,
+    content => 'Hello World!\n',
+    require => Package['nginx'],
+  }
+
+  file { '/var/www/html/404.html':
+    ensure  => file,
+    content => "Ceci n'est pas une page\n",
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => template('nginx/default.conf.erb'),
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/nginx/sites-available/default'],
+  }
 }
 
-exec {'install':
-  provider => shell,
-  path     => '/usr/bin:/usr/sbin:/bin',
-  command  => 'sudo apt-get -y install nginx',
-}
-
-exec {'echo_html':
-  provider => shell,
-  path     => '/usr/bin:/usr/sbin:/bin',
-  command  => 'sudo echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
-}
-
-exec {'sed_config':
-  command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.youtube.com permanent;" /etc/nginx/sites-available/default',
-  provider => shell,
-  path     => '/usr/bin:/usr/sbin:/bin',
-}
-
-exec {'start':
-  command  => 'sudo service nginx start',
-  provider => shell,
-  path     => '/usr/bin:/usr/sbin:/bin',
-}
+include nginx_setup
