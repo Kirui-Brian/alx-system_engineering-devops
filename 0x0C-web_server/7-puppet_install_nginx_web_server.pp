f8e16707baecfadf696b91b2c7df1b54843e1c4f
Kirui Brian <brinkiru@gmail.com>
2024-05-16 10:27:57 +0300

    update  7-puppet_install_nginx_web_server.pp file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,46 +1,30 @@
-# This Puppet manifest installs and configures Nginx to serve a 'Hello World!' page and redirect '/redirect_me' with a 301 response.
-class nginx_setup {
-  package { 'nginx':
-    ensure => installed,
-    before => File['/var/www/html/index.html'],
-  }
-
-  service { 'nginx':
-    ensure     => running,
-    enable     => true,
-    require    => Package['nginx'],
-    subscribe  => File['/var/www/html/index.html', '/etc/nginx/sites-available/default'],
-  }
-
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => 'Hello World!\n',
-    require => Package['nginx'],
-  }
-
-  file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => template('nginx/default.erb'),
-    require => Package['nginx'],
-  }
+# Script that installs and configures Nginx
+exec {'update':
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
+  command  => 'sudo apt-get -y update',
 }
 
-include nginx_setup
+exec {'install':
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
+  command  => 'sudo apt-get -y install nginx',
+}
 
-# Template file: nginx/default.erb
-# Place this template in the appropriate Puppet module directory
-<% | $redirect_url = 'http://google.com/' |%>
-server {
-  listen 80;
-  listen [::]:80 default_server;
-  root   /var/www/html;
-  index  index.html index.htm index.nginx-debian.html;
+exec {'echo_html':
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
+  command  => 'sudo echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
+}
 
-  location / {
-    try_files $uri $uri/ =404;
-  }
+exec {'sed_config':
+  command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.youtube.com permanent;" /etc/nginx/sites-available/default',
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
+}
 
-  location /redirect_me {
-    return 301 <%= $redirect_url %>;
-  }
+exec {'start':
+  command  => 'sudo service nginx start',
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
 }
