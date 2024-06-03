1a835f134acebd5282136e6903fad41784fc42ea
Kirui Brian <brinkiru@gmail.com>
2024-06-04 01:17:46 +0300

    add 1-to_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+def top_ten(subreddit):
+    """
+    Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'custom'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print(None)
+        return
+    data = response.json().get("data").get("children")
+    for post in data:
+        print(post.get("data").get("title"))
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
